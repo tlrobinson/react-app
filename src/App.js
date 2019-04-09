dd5d086aecfa4bad4b84b989be04eea43616dcd9
Tom Robinson <tlrobinson@gmail.com>
2019-04-09 02:57:18 -0700

    hot reload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -25,4 +27,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
